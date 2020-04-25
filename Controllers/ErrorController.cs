f6c8ed255eb0ba1516bb2c1db8490e9043aa033f
ga_96 <giannianfiteatro@gmail.com>
2020-04-25 13:01:31 +0930

    Initial Source Code Commit
    Initial Source Code Commit

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BrunosTipperHire.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        [HandleError]
+        public ActionResult Error404()
+        {
+            return View();
+        }
+
+        [HandleError]
+        public ActionResult Error500()
+        {
+            return View();
+        }
+
+        [HandleError]
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
